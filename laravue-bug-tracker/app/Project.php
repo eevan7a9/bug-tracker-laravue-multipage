86db973b5d10ed657159107e13838db66d63b017
eevan7a9 <e7a9@protonmail.com>
2019-12-22 14:30:29 +0900

    added fillables

--- laravue-bug-tracker/app/Project.php
+++ laravue-bug-tracker/app/Project.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'version',
+        'type',
+        'os',
+        'description',
+        'development_start',
+        'release_date',
+    ];
 }
