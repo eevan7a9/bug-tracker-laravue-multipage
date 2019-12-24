0e7b1d3e6058955545a805a5b4dcbd71bb4600e9
eevan7a9 <e7a9@protonmail.com>
2019-12-24 15:46:44 +0900

    registered users automaticaly be given the role of customer

--- laravue-bug-tracker/app/Http/Controllers/Auth/RegisterController.php
+++ laravue-bug-tracker/app/Http/Controllers/Auth/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
+use App\Role;
 use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
@@ -20,7 +21,7 @@ class RegisterController extends Controller
     | validation and creation. By default this controller uses a trait to
     | provide this functionality without requiring any additional code.
     |
-    */
+     */
 
     use RegistersUsers;
 
@@ -64,10 +65,14 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+
+        $role = Role::first();
+        $user->roles()->attach($role);
+        return $user;
     }
 }
