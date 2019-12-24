76bca36639396c39ff048fb4396d74ad07139104
eevan7a9 <e7a9@protonmail.com>
2019-12-24 15:44:57 +0900

    added RoleSeeders

---
+++ laravue-bug-tracker/database/seeds/RoleSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('roles')->insert([
+            "id" => 1,
+            "name" => "customer",
+            "created_at" => now(),
+            "updated_at" => now(),
+        ]);
+
+        DB::table('roles')->insert([
+            "id" => 2,
+            "name" => "developer",
+            "created_at" => now(),
+            "updated_at" => now(),
+        ]);
+
+        DB::table('roles')->insert([
+            "id" => 3,
+            "name" => "admin",
+            "created_at" => now(),
+            "updated_at" => now(),
+        ]);
+    }
+
+}
