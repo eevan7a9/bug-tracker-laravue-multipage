9523ae27776c3bbe1adb65aa561eb82ce71a8c61
eevan7a9 <e7a9@protonmail.com>
2019-12-24 14:37:37 +0900

    edit layout

--- laravue-bug-tracker/resources/js/components/AddProject.vue
+++ laravue-bug-tracker/resources/js/components/AddProject.vue
@@ -129,7 +129,23 @@
                 </div>
               </div>
               <div class="text-right">
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-primary btn-lg">
+                  Submit
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  >
+                    <line x1="22" y1="2" x2="11" y2="13" />
+                    <polygon points="22 2 15 22 11 13 2 9 22 2" />
+                  </svg>
+                </button>
               </div>
             </div>
           </div>
