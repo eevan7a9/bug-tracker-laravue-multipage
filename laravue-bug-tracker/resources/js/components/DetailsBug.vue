cb7140bab76bfbcb2650a317ce2d8dca79f16834
eevan7a9 <e7a9@protonmail.com>
2019-12-26 10:13:12 +0900

    aded colapse image

--- laravue-bug-tracker/resources/js/components/DetailsBug.vue
+++ laravue-bug-tracker/resources/js/components/DetailsBug.vue
@@ -2,15 +2,43 @@
   <div>
     <div class="card">
       <div class="card-header">
-        <button class="btn btn-primary" @click="hideDetails">Back</button>
+        <button class="btn btn-outline-secondary" @click="hideDetails">Back</button>
       </div>
       <div class="card-body">
-        <div class="image-container text-center w-100">
-          <img src="https://via.placeholder.com/450" alt />
-        </div>
         <div class="row justify-content-center">
           <div class="col-md-10">
-            <h4 class="card-title py-2 text-center">{{ bug_details.title }}</h4>
+            <b-button
+              :class="visible_screenshot ? null : 'collapsed'"
+              :aria-expanded="visible_screenshot ? 'true' : 'false'"
+              aria-controls="collapse-4"
+              variant="outline-primary"
+              @click="visible_screenshot = !visible_screenshot"
+            >
+              Show Screenshot
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <rect x="3" y="3" width="18" height="18" rx="2" ry="2" />
+                <circle cx="8.5" cy="8.5" r="1.5" />
+                <polyline points="21 15 16 10 5 21" />
+              </svg>
+            </b-button>
+            <b-collapse id="collapse-4" v-model="visible_screenshot" class="mt-2">
+              <b-card>
+                <div class="image-container text-center w-100">
+                  <img src="https://via.placeholder.com/750" alt />
+                </div>
+              </b-card>
+            </b-collapse>
+            <h4 class="card-title py-2 text-left">{{ bug_details.title }}</h4>
             <div class="d-flex justify-content-md-between align-content-center">
               <h5 class="py-2 font-weight-bold">
                 <small>Project Name :</small>
@@ -35,7 +63,9 @@
                   header="Severity"
                   class="text-center"
                 >
-                  <b-card-text>{{ bug_details.severity }}</b-card-text>
+                  <b-card-text>
+                    <span class="font-weight-bold text-capitalize">{{ bug_details.severity }}</span>
+                  </b-card-text>
                 </b-card>
                 <!-- <p class="mt-3 mb-0 text-capitalize">{{ bug_details.severity }}</p>
                 <small class="text-danger">Severity</small>-->
@@ -49,7 +79,9 @@
                   header="Priority"
                   class="text-center"
                 >
-                  <b-card-text>{{ bug_details.priority }}</b-card-text>
+                  <b-card-text>
+                    <span class="font-weight-bold text-capitalize">{{ bug_details.priority }}</span>
+                  </b-card-text>
                 </b-card>
                 <!-- <p class="mt-3 mb-0 text-capitalize">{{ bug_details.priority }}</p>
                 <small class="text-success">Priority</small>-->
@@ -63,7 +95,11 @@
                   header="Assigned To"
                   class="text-center"
                 >
-                  <b-card-text>{{ bug_details.assigned_to ? bug_details.assigned_to.email : "Unassigned" }}</b-card-text>
+                  <b-card-text>
+                    <span
+                      class="font-weight-bold"
+                    >{{ bug_details.assigned_to ? bug_details.assigned_to.email : "Unassigned" }}</span>
+                  </b-card-text>
                 </b-card>
                 <!-- <p
                   class="mt-3 mb-0 text-capitalize"
@@ -81,7 +117,9 @@
                   header="Browser"
                   class="text-center"
                 >
-                  <b-card-text>{{ bug_details.browser }}</b-card-text>
+                  <b-card-text>
+                    <span class="font-weight-bold text-capitalize">{{ bug_details.browser }}</span>
+                  </b-card-text>
                 </b-card>
                 <!-- <h5 class="mt-3 mb-0 font-weight-bold">{{ bug_details.browser }}</h5>
                 <small>Browser</small>-->
@@ -95,7 +133,9 @@
                   header="Os"
                   class="text-center"
                 >
-                  <b-card-text>{{ bug_details.os }}</b-card-text>
+                  <b-card-text>
+                    <span class="font-weight-bold text-capitalize">{{ bug_details.os }}</span>
+                  </b-card-text>
                 </b-card>
                 <!-- <h5 class="mt-3 mb-0 font-weight-bold">{{ bug_details.os }}</h5>
                 <small>Operating System</small>-->
@@ -109,7 +149,9 @@
                   header="Bug Type"
                   class="text-center"
                 >
-                  <b-card-text>{{ bug_details.bug_type }}</b-card-text>
+                  <b-card-text>
+                    <span class="font-weight-bold text-capitalize">{{ bug_details.bug_type }}</span>
+                  </b-card-text>
                 </b-card>
                 <!-- <h5 class="mt-3 mb-0 font-weight-bold">{{ bug_details.bug_type }}</h5>
                 <small>Bug Type</small>-->
@@ -126,6 +168,11 @@
 <script>
 import { mapGetters, mapActions } from "vuex";
 export default {
+  data() {
+    return {
+      visible_screenshot: false
+    };
+  },
   computed: mapGetters(["bug_details"]),
   methods: {
     ...mapActions(["clearBugDetails"]),
