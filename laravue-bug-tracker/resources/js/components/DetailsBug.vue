803e2dea9fc636c6fe1c5571fb6d7dd6bd63419b
eevan7a9 <e7a9@protonmail.com>
2019-12-26 09:12:31 +0900

    change layout add  cards

--- laravue-bug-tracker/resources/js/components/DetailsBug.vue
+++ laravue-bug-tracker/resources/js/components/DetailsBug.vue
@@ -9,7 +9,7 @@
           <img src="https://via.placeholder.com/450" alt />
         </div>
         <div class="row justify-content-center">
-          <div class="col-md-8">
+          <div class="col-md-10">
             <h4 class="card-title py-2 text-center">{{ bug_details.title }}</h4>
             <div class="d-flex justify-content-md-between align-content-center">
               <h5 class="py-2 font-weight-bold">
@@ -21,37 +21,98 @@
                 {{ bug_details.created_at }}
               </h5>
             </div>
-            <div class="card p-3 bg-light">
+
+            <div class="card p-3 bg-light mt-3">
               <p class="card-text bg-light">{{ bug_details.description }}</p>
             </div>
-            <div class="row">
+            <div class="row mt-3">
               <div class="col-md-4 text-center">
-                <p class="mt-3 mb-0 text-capitalize">{{ bug_details.severity }}</p>
-                <small class="text-danger">Severity</small>
+                <b-card
+                  bg-variant="light"
+                  header-bg-variant="primary"
+                  header-text-variant="white"
+                  text-variant="dark"
+                  header="Severity"
+                  class="text-center"
+                >
+                  <b-card-text>{{ bug_details.severity }}</b-card-text>
+                </b-card>
+                <!-- <p class="mt-3 mb-0 text-capitalize">{{ bug_details.severity }}</p>
+                <small class="text-danger">Severity</small>-->
               </div>
               <div class="col-md-4 text-center">
-                <p class="mt-3 mb-0 text-capitalize">{{ bug_details.priority }}</p>
-                <small class="text-success">Priority</small>
+                <b-card
+                  bg-variant="light"
+                  header-bg-variant="primary"
+                  header-text-variant="white"
+                  text-variant="dark"
+                  header="Priority"
+                  class="text-center"
+                >
+                  <b-card-text>{{ bug_details.priority }}</b-card-text>
+                </b-card>
+                <!-- <p class="mt-3 mb-0 text-capitalize">{{ bug_details.priority }}</p>
+                <small class="text-success">Priority</small>-->
               </div>
               <div class="col-md-4 text-center">
-                <p
+                <b-card
+                  bg-variant="light"
+                  header-bg-variant="primary"
+                  header-text-variant="white"
+                  text-variant="dark"
+                  header="Assigned To"
+                  class="text-center"
+                >
+                  <b-card-text>{{ bug_details.assigned_to ? bug_details.assigned_to.email : "Unassigned" }}</b-card-text>
+                </b-card>
+                <!-- <p
                   class="mt-3 mb-0 text-capitalize"
                 >{{ bug_details.assigned_to ? bug_details.assigned_to.email : "Unassigned" }}</p>
-                <small class="text-primary">Assigned To</small>
+                <small class="text-primary">Assigned To</small>-->
               </div>
             </div>
-            <div class="row">
+            <div class="row mt-3">
               <div class="col-md-4 text-center">
-                <h5 class="mt-3 mb-0 font-weight-bold">{{ bug_details.browser }}</h5>
-                <small>Browser</small>
+                <b-card
+                  bg-variant="light"
+                  header-bg-variant="primary"
+                  header-text-variant="white"
+                  text-variant="dark"
+                  header="Browser"
+                  class="text-center"
+                >
+                  <b-card-text>{{ bug_details.browser }}</b-card-text>
+                </b-card>
+                <!-- <h5 class="mt-3 mb-0 font-weight-bold">{{ bug_details.browser }}</h5>
+                <small>Browser</small>-->
               </div>
               <div class="col-md-4 text-center">
-                <h5 class="mt-3 mb-0 font-weight-bold">{{ bug_details.os }}</h5>
-                <small>Operating System</small>
+                <b-card
+                  bg-variant="light"
+                  header-bg-variant="primary"
+                  header-text-variant="white"
+                  text-variant="dark"
+                  header="Os"
+                  class="text-center"
+                >
+                  <b-card-text>{{ bug_details.os }}</b-card-text>
+                </b-card>
+                <!-- <h5 class="mt-3 mb-0 font-weight-bold">{{ bug_details.os }}</h5>
+                <small>Operating System</small>-->
               </div>
               <div class="col-md-4 text-center">
-                <h5 class="mt-3 mb-0 font-weight-bold">{{ bug_details.bug_type }}</h5>
-                <small>Bug Type</small>
+                <b-card
+                  bg-variant="light"
+                  header-bg-variant="primary"
+                  header-text-variant="white"
+                  text-variant="dark"
+                  header="Bug Type"
+                  class="text-center"
+                >
+                  <b-card-text>{{ bug_details.bug_type }}</b-card-text>
+                </b-card>
+                <!-- <h5 class="mt-3 mb-0 font-weight-bold">{{ bug_details.bug_type }}</h5>
+                <small>Bug Type</small>-->
               </div>
             </div>
           </div>
