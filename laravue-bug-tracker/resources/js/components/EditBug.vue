e1109d5f84fecc255ca1d810a269e2017c6e1922
eevan7a9 <e7a9@protonmail.com>
2019-12-28 22:16:31 +0900

    fixed error when editing image of unassigned

--- laravue-bug-tracker/resources/js/components/EditBug.vue
+++ laravue-bug-tracker/resources/js/components/EditBug.vue
@@ -294,9 +294,9 @@ export default {
     this.bug.type = this.bug_details.bug_type;
     this.bug.severity = this.bug_details.severity;
     this.bug.priority = this.bug_details.priority;
-    this.bug.developer = this.bug_details.assigned_to
-      ? this.bug_details.assigned_to.id
-      : "";
+    this.bug_details.assigned_to
+      ? (this.bug.developer = this.bug_details.assigned_to.id)
+      : null;
   },
   created() {
     if (this.projects.length == 0 && this.developers.length == 0) {
