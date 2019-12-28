a03f439bf4cd4ba838118356dd6b6be986c777e8
eevan7a9 <e7a9@protonmail.com>
2019-12-28 10:05:11 +0900

    change auth styles

--- laravue-bug-tracker/resources/views/auth/register.blade.php
+++ laravue-bug-tracker/resources/views/auth/register.blade.php
@@ -4,8 +4,8 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+            <div class="card border-primary">
+                <div class="card-header bg-primary text-light">{{ __('Register') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
