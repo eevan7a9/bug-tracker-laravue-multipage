2a84dfebaf91f7c1c16800c917902d7922153328
eevan7a9 <e7a9@protonmail.com>
2019-12-29 13:46:52 +0900

    added comment model and migration

---
+++ laravue-bug-tracker/database/migrations/2019_12_29_031835_create_comments_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('bug_id');
+            $table->longText('message');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')
+                ->on('bugs')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->foreign('bug_id')->references('id')
+                ->on('bugs')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
